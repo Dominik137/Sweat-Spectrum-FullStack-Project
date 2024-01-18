849cd73e77cfdd799953272f57322db3b48135fb
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-01-18 16:31:27 -0700

    add home module

--- Frontend/SweatSpectrum/src/App.jsx
+++ Frontend/SweatSpectrum/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LogIn from './components/login'
+import Home from './components/home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <LogIn />
+        <Home />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ Frontend/SweatSpectrum/src/components/home.jsx
@@ -0,0 +1,11 @@
+import LogIn from "./login";
+
+function Home(){
+    return (
+        <div>
+            <LogIn/>
+        </div>
+    )
+}
+
+export default Home;
