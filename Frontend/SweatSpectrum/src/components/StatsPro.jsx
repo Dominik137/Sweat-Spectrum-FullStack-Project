7b7e9dddfdbeca23af60acb93dab13bcbe672e58
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-01-23 15:37:00 -0700

    install chart.js and begin dummy data

--- Frontend/SweatSpectrum/src/components/StatsPro.jsx
+++ Frontend/SweatSpectrum/src/components/StatsPro.jsx
@@ -1,12 +1,66 @@
 import Analytics from "./Analytics"
+import {useState, useEffect} from "react"
+import {Chart as ChartJS} from "chart.js/auto"
+import {Bar} from "react-chartjs-2"
 
-function StatsPro(){
+function StatsPro({user}){
+    const [userWorkouts, setUserWorkouts] = useState([])
+
+    useEffect(() => {
+      if (user) {
+        fetch(`/api/users/${user.id}/workouts`)
+          .then(r => r.json())
+          .then(data => setUserWorkouts(data))
+      }
+    }, [user]);
+
+    //All dates of user runs
+    //All distances of user runs 
+
+
+
     return(
         //This is the page that will display the graphs for the different workout types a user has logged, as well as link to detailed breakdown per workout type.
         <div>
             <h1>StatsPro</h1>
             //Graphs go here
             <button> LINK TO ANALYTICS PAGE HERE</button>
+
+            <h2>Runs</h2>
+
+            <article className="run-chart-distance"> 
+                <Bar 
+                    data={{
+                        labels: ["2024-01-02", "2024-01-12", "2024-01-23"],
+                        datasets: [ 
+                            {
+                                label: "Distance",
+                                data: [9.5, 3.5, 10.5],
+                            },
+
+                        ],
+                    }}
+                />
+            </article>
+
+            <article className="run-chart-hr"> 
+                <Bar 
+                    data={{
+                        labels: ["2024-01-02", "2024-01-12", "2024-01-23"],
+                        datasets: [ 
+                            {
+                                label: "Avg. Heart Rate",
+                                data: [120, 130, 140],
+                            },
+                            {
+                                label: "Max. Heart Rate",
+                                data: [156, 164, 180],
+                            },
+
+                        ],
+                    }}
+                />
+            </article>
         </div>
     )
 }
