39adaa5762309f69c4e78e307ed8d4568749b41d
Dominik Arment <domarment@gmail.com>
2024-01-24 12:38:59 -0700

    doms stuff

--- Frontend/SweatSpectrum/src/components/EditWorkoutForm.jsx
+++ Frontend/SweatSpectrum/src/components/EditWorkoutForm.jsx
@@ -1,100 +1,230 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function EditWorkoutForm({ user }) {
-  const params = useParams();
-  const workoutId = params.id;
-  const navigate = useNavigate();
-  const [existingWorkout, setExistingWorkout] = useState(null);
-
-  const [workoutDetails, setWorkoutDetails] = useState({
+ const params = useParams();
+ const workoutId = params.id;
+ const navigate = useNavigate();
+ const [existingWorkout, setExistingWorkout] = useState(null);
+ const [workoutDetails, setWorkoutDetails] = useState({
     type: '',
     hours: 0,
     minutes: 0,
     seconds: 0,
-    date: new Date().toISOString().split('T')[0], // Initialize date in 'YYYY-MM-DD' format
-    time: '',
+    date: new Date(),
+    time: "",
     attributes: {},
-  });
+ });
 
-  useEffect(() => {
-    const fetchWorkoutDetails = async () => {
-      try {
-        const response = await fetch(`/api/users/${user?.id}/workouts/${workoutId}`);
-        if (response.ok) {
-          const data = await response.json();
-          setExistingWorkout(data);
-          setWorkoutDetails({
-            type: data.type,
-            hours: data.hours,
-            minutes: data.minutes,
-            seconds: data.seconds,
-            date: new Date(data.date).toISOString().split('T')[0], // Format date from response
-            time: data.time,
-            attributes: data.attributes,
-          });
-        } else {
-          // Handle error
-          console.error('Failed to fetch workout details');
-        }
-      } catch (error) {
-        console.error('Error during fetch:', error);
-      }
-    };
+ const workoutAttributes = {
+    Run: ['distance', 'pace', 'active calories', 'total calories', 'avg heart rate', 'max heart rate', 'elevation gain', 'notes'],
+    Bike: ['distance', 'pace', 'active calories', 'total calories', 'avg heart rate', 'max heart rate', 'elevation gain', 'notes'],
+    Swim: ['type', 'stroke', 'distance', 'laps', 'active calories', 'total calories', 'avg heart rate', 'max heart rate', 'notes'],
+    "Weight Training": ['muscle group', 'type', 'weight', 'reps', 'sets', 'active calories', 'total calories', 'avg heart rate', 'max heart rate', 'notes'],
+    HIIT: ['intensity', 'rounds', 'warm up', 'cool down', 'muscle group', 'active calories', 'total calories', 'notes'],
+ };
 
-    fetchWorkoutDetails();
-  }, [user.id, workoutId]);
-
-  const handlePatch = async () => {
+ const fetchWorkoutDetails = async () => {
     try {
-      const response = await fetch(`/api/users/${user.id}/workouts/${workoutId}`, {
+      const response = await fetch(`/api/users/${user?.id}/workouts/${workoutId}`);
+      const data = await response.json();
+      setExistingWorkout(data);
+
+      prefillForm(data);
+    } catch (error) {
+      console.error('Error fetching workout details:', error);
+    }
+ };
+
+const prefillForm = (data) => {
+  // console.log(data);
+  const durationParts = data.duration.split(':');
+  const parsedAttributes = JSON.parse(data.attributes || '{}');
+  setWorkoutDetails({
+    type: data.type,
+    hours: parseInt(durationParts[0], 10),
+    minutes: parseInt(durationParts[1], 10),
+    seconds: parseInt(durationParts[2], 10),
+    date: new Date(data.date),
+    time: data.time,
+    attributes: parsedAttributes
+  }); 
+};
+
+ useEffect(() => {
+    fetchWorkoutDetails();
+ }, [user?.id, workoutId]);
+
+ const handleChange = (event) => {
+    const { name, value } = event.target;
+    let parsedValue = event.target.type === 'number' ? parseInt(value, 10) : value;
+
+    if (name === 'time') {
+      parsedValue = value; // Store the time input value as a string
+    }
+
+    if (name.includes('attributes.')) {
+      const attributeName = name.split('.')[1];
+      setWorkoutDetails((prevDetails) => ({
+        ...prevDetails,
+        attributes: {
+          ...prevDetails.attributes,
+          [attributeName]: parsedValue,
+        },
+      }));
+    } else {
+      setWorkoutDetails((prevDetails) => ({
+        ...prevDetails,
+        [name]: parsedValue,
+      }));
+    }
+ };
+
+ const handlePatchWorkout = async () => {
+    try {
+      const attributes = {};
+      if (workoutDetails.type && workoutAttributes[workoutDetails.type]) {
+        workoutAttributes[workoutDetails.type].forEach(attribute => {
+          attributes[attribute] = workoutDetails.attributes[attribute] || '';
+        });
+      }
+
+      if (workoutDetails.minutes < 0 || workoutDetails.minutes > 59 ||
+        workoutDetails.seconds < 0 || workoutDetails.seconds > 59) {
+        alert("Please enter valid minutes and seconds (0-59).");
+        return;
+      }
+
+      const timeParts = workoutDetails.time.split(':');
+      const timeHours = parseInt(timeParts[0], 10);
+      const timeMinutes = parseInt(timeParts[1], 10);
+      const time = new Date();
+      time.setHours(timeHours);
+      time.setMinutes(timeMinutes);
+
+      const formattedTime = time.getHours().toString().padStart(2, '0') + ':' + time.getMinutes().toString().padStart(2, '0') + ':00';
+
+      const formattedHours = workoutDetails.hours ? workoutDetails.hours.toString().padStart(2, '0') : '00';
+      const formattedMinutes = workoutDetails.minutes ? workoutDetails.minutes.toString().padStart(2, '0') : '00';
+      const formattedSeconds = workoutDetails.seconds ? workoutDetails.seconds.toString().padStart(2, '0') : '00';
+
+      await fetch(`/api/users/${user?.id}/workouts/${workoutId}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          ...workoutDetails,
-          date: new Date(workoutDetails.date).toISOString().split('T')[0], // Format date before sending
+          type: workoutDetails.type,
+          duration:`${formattedHours}:${formattedMinutes}:${formattedSeconds}`,
+          date: workoutDetails.date.toISOString().split('T')[0],
+          time: formattedTime,
+          attributes: JSON.stringify(attributes),
         }),
       });
 
-      if (response.ok) {
-        // Handle successful update
-        console.log('Workout updated successfully');
-        navigate('/dashboard'); // Redirect to the dashboard or wherever you need
-      } else {
-        // Handle error
-        console.error('Failed to update workout');
-      }
+      navigate('/dashboard');
     } catch (error) {
-      console.error('Error during update:', error);
+      console.error('Error updating workout details:', error);
     }
-  };
+ };
+
+ const renderInputsBasedOnType = () => {
+  const type = existingWorkout?.type;
+
+  if (type && workoutAttributes[type]) {
+    return workoutAttributes[type].map((attribute) => (
+      <div key={attribute}>
+        <label htmlFor={attribute}>{attribute}:</label>
+        <input
+          type="text"
+          id={attribute}
+          value={workoutDetails.attributes[attribute] || ''}
+          onChange={(e) => setWorkoutDetails((prevDetails) => ({
+            ...prevDetails,
+            attributes: { ...prevDetails.attributes, [attribute]: e.target.value },
+          }))}
+        />
+      </div>
+    ));
+  }
+
+  return null;
+};
+
 
   return (
     <div>
-      <h2>Edit Workout</h2>
-      <form>
-        <label>Type:</label>
-        <input
-          type="text"
-          value={workoutDetails.type}
-          onChange={(e) => setWorkoutDetails({ ...workoutDetails, type: e.target.value })}
-        />
+      <label htmlFor="type">Type:</label>
+      <input type="text" id="type" value={workoutDetails.type} readOnly />
 
-        <label>Date:</label>
-        <input
-          type="date"
-          value={workoutDetails.date}
-          onChange={(e) => setWorkoutDetails({ ...workoutDetails, date: e.target.value })}
-        />
+      {/* Render common workout details input fields */}
+      
+      <label>
+  Duration:
+  <div>
+    <input
+      type="number"
+      name="hours"
+      value={workoutDetails.hours}
+      onChange={handleChange}
+      placeholder="Hours"
+      min="0"
+      max="23"
+      required
+    />
+    :
+    <input
+      type="number"
+      name="minutes"
+      value={workoutDetails.minutes}
+      onChange={handleChange}
+      placeholder="Minutes"
+      min="0"
+      max="59"
+      required
+    />
+    :
+    <input
+      type="number"
+      name="seconds"
+      value={workoutDetails.seconds}
+      onChange={handleChange}
+      placeholder="Seconds"
+      min="0"
+      max="59"
+      required
+    />
+  </div>
+</label>
+<br />
+<label>
+  Date:
+  <DatePicker
+    selected={new Date(workoutDetails.date)}
+    onChange={(date) => setWorkoutDetails((prevDetails) => ({ ...prevDetails, date }))}
+  />
+</label>
+<br />
+<label htmlFor="time">
+  Time:
+  <input
+    type="time"
+    id="time"
+    name="time"
+    value={workoutDetails.time}
+    onChange={handleChange}
+  />
+</label>
+<br />
 
-        {/* Include other input fields for hours, minutes, seconds, time, etc. */}
-        
-        <button type="button" onClick={handlePatch}>
-          Update Workout
-        </button>
-      </form>
+
+      {/* Render inputs based on workout type */}
+      {renderInputsBasedOnType()}
+
+      {/* Button to trigger the update */}
+      <button onClick={handlePatchWorkout}>Update Workout</button>
     </div>
   );
 }
